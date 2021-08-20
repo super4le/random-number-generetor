8fc9e908d5a6914751f756f2ba1cb98fc86dec82
super4le <alessandracursio@hotmail.it>
2021-08-20 15:32:31 +0200

    feat: backend working

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Random number generetor
 
 This is a simple fullstack random number generetor program developed by super4le (me) during the Lacerba node and typescript development course
+
+## Install
+
+```bash
+npm install
+```
+
+## Usage backend
+
+```bash
+ts-node main.ts
+```
+example output:
+"Bari": [
+    20,
+    27,
+    45,
+    93,
+    2
+  ],
--- main.ts
+++ main.ts
@@ -1,1 +1,37 @@
-console.log('hello world');
+function RNG(min: number, max: number) {
+    const rng = Math.random();
+
+    // rng * (max - min) + min
+    // rng = 0 -> min
+    // rng = 1 -> max
+
+    return Math.trunc(rng * (max - min) + min);
+}
+
+function RNGSequence(len: number, min: number, max: number) {
+    if (len > max - min) {
+        throw new Error(`cannot find ${len} number between ${min} and ${max}`);
+    }
+    const res: number[] = [];
+    while (res.length < len) {
+        const rn = RNG(min, max);
+        if (res.includes(rn)) {
+            continue;
+        }
+        res.push(rn);
+    }
+    return res;
+}
+
+const ruote = ['Bari', 'Cagliari', 'Firenze', 'Genova', 'Milano', 'Napoli', 'Palermo', 'Roma', 'Torino', 'Venezia', 'Nazionale'];
+
+const estrazioni: { [ruota: string]: number[] } = {};
+
+for (const ruota of ruote){
+    const estrazione = RNGSequence(5, 1, 100);
+    estrazioni[ruota] = estrazione;
+}
+
+console.log(JSON.stringify(estrazioni, null, 2));
+
+
